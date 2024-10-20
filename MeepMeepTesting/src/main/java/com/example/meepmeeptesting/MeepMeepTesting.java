67e5bbcd611313c51eddd63ff61a1e7757e38940
laasimodem <laasimodem2@gmail.com>
2024-10-20 11:41:22 -0400

    Update MeepMeepTesting.java
    auton - RightLeft - 37
    Step1. SAMPLE in HIGH CHAMBER 10 
    Step2 SAMPLE in HIGH BASKET 8 
    Step3 SAMPLE in HIGH BASKET 8 
    Step4 SAMPLE in HIGH BASKET 8
    Step5 LEVEL 1 ASCENT - 3

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -21,41 +21,50 @@ public class MeepMeepTesting {
                 .setConstraints(100, 100, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(24, -64, Math.toRadians(180)))
-                .splineToConstantHeading(new Vector2d(0,-30),Math.toRadians(90))
-                        .waitSeconds(2)
-                                .lineToX(45)
-
-
-
-                .splineToConstantHeading(new Vector2d(-52,-60),Math.toRadians(180))
-
-                    .strafeTo(new Vector2d(57, -34))//, Math.toRadians(90))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-6, -60, Math.toRadians(180)))
+                .strafeTo(new Vector2d(-6, -45))// Math.toRadians(90))
                 .waitSeconds(2)
 
 
+
+
+                .splineToConstantHeading(new Vector2d(-48,-34),Math.toRadians(180))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(-60, -60))//, Math.toRadians(90))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(-58, -34))//, Math.toRadians(90))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(-60, -60))//, Math.toRadians(90))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(-60, -23))//, Math.toRadians(90))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(-60, -60))//, Math.toRadians(90))
+                        .waitSeconds(1)
+                .strafeTo(new Vector2d(-25, -10))//, Math.toRadians(90))
+                .waitSeconds(1)
+
+               /* .splineToConstantHeading(new Vector2d(-52,-60),Math.toRadians(180))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(50, -20))// Math.toRadians(90))
+                .waitSeconds(1)
                 .splineToConstantHeading(new Vector2d(-52,-60),Math.toRadians(180))
                 .waitSeconds(1)
-                           .strafeTo(new Vector2d(50, -20))// Math.toRadians(90))
-                .waitSeconds(1)
-                .splineToConstantHeading(new Vector2d(-52,-60),Math.toRadians(180))
-                .waitSeconds(1)
-                             .strafeTo(new Vector2d(54, -64))// Math.toRadians(180))
-                .waitSeconds(1)
+                .strafeTo(new Vector2d(54, -64))// Math.toRadians(180))
+                .waitSeconds(1) */
 
 //                        .waitSeconds(2)
-                          /*      .lineToX(30)
+                /*      .lineToX(30)
 
-               /* .splineTo(new Vector2d(38, -55), Math.toRadians(180))
-                        .waitSeconds(2)
-                .splineTo(new Vector2d(53, -50), Math.toRadians(310))
-                        .waitSeconds(2)
-                .splineTo(new Vector2d(24, -55), Math.toRadians(270))
-                                .waitSeconds(2)
-                        .lineToY(-50)
-                .splineTo(new Vector2d(53, -50), Math.toRadians(130))
+     /* .splineTo(new Vector2d(38, -55), Math.toRadians(180))
+              .waitSeconds(2)
+      .splineTo(new Vector2d(53, -50), Math.toRadians(310))
+              .waitSeconds(2)
+      .splineTo(new Vector2d(24, -55), Math.toRadians(270))
+                      .waitSeconds(2)
+              .lineToY(-50)
+      .splineTo(new Vector2d(53, -50), Math.toRadians(130))
 
-           */
+ */
 
 
 
@@ -65,16 +74,16 @@ public class MeepMeepTesting {
                 //.lineToX(32)
                 //.setTangent(Math.toRadians(0))
                 //.splineTo(new Vector2d(44,28), Math.toRadians(0))
-                        .build());
+                .build());
 
         Image img = null;
         try { img = ImageIO.read(new File("C:\\Users\\rgutti1\\Documents\\PERSONAL\\LAASYA\\ROBOTICS\\intothedeep.png")); }
         catch (IOException e) {}
 
         meepMeep.setBackground(img)
-                        .setBackgroundAlpha(0.95f)
-                                .addEntity(myBot)
-                                        .start();
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
 //  <following code you were using previously>
 
     }
